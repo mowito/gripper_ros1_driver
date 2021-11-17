ebe73b5b3418bf67f3b6d9fca8d92ff6d60c307e
mclaren <mclaren@mowito>
2021-11-17 22:17:11 +0530

    change ip

--- robotiq_vacuum_grippers_control/scripts/host_socat.sh
+++ robotiq_vacuum_grippers_control/scripts/host_socat.sh
@@ -2,6 +2,6 @@
 
 # starting the socat in the pendant
 echo "going to run socat on  host"
-socat pty,link=/dev/ttyTool,raw,ignoreeof,waitslave tcp:192.168.0.1:54321&
+socat pty,link=/dev/ttyTool,raw,ignoreeof,waitslave tcp:10.10.10.1:54321&
 sleep .5
 chmod 777 /dev/ttyTool
