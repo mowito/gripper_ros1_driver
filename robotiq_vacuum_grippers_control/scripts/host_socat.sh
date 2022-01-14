a41080ad1568450cfcdc20406e2ec939d6995703
Kewal Bajaj <kewal@mowito.in>
2022-01-14 15:20:52 +0530

    sv -- folder structure changed

--- robotiq_vacuum_grippers_control/scripts/host_socat.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# starting the socat in the pendant
-echo "going to run socat on  host"
-socat pty,link=/dev/ttyTool,raw,ignoreeof,waitslave tcp:10.10.10.1:54321&
-sleep .5
-chmod 777 /dev/ttyTool
--- robotiq_vacuum_grippers_control/scripts/tp_socat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# starting the socat in the pendant
-echo "going to ssh into pendan and run socat"
-ssh root@192.168.0.1 'cd mowito_ur_test && ./socat tcp-l:54321,reuseaddr,fork file:/dev/ttyTool,nonblock,raw,waitlock=/var/run/tty'&
